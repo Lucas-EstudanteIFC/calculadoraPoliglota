280aa42f2e4a9145f646090e4a0e8de126cc7d49
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2023-11-16 09:29:51 -0300

    commit 16/11/23

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
+let idioma = document.getElementByID("idiomas");
+let boasVindas = document.getElementById("boas-vindas");
 
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
+idioma.addEventListener("change", function () {
+    if (this.value == "br") {
+        btSoma.value = "Somar";
+        btSubratrai.value = "Subtrair";
+        btMultiplica.value = "Multiplicar";
+        btDividi.value = "Dividir";
+    }
+    else if (this.value == "en") {
+        btSoma.value = "Add";
+        btSubratrai.value = "Subtract";
+        btMultiplica.value = "Multiply";
+        btDividi.value = "Divide";
+    }
+    else if (this.value == "es") {
+        btSoma.value = "Agregar";
+        btSubratrai.value = "Sustraer";
+        btMultiplica.value = "Multiplicar";
+        btDividi.value = "Dividir";
+    }
+})
 
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
 
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
+let btSoma = document.getElementById("btSomar");
+btSoma.addEventListener("click", function () {
     somar(Number(valor1.value), Number(valor2.value));
 });
 
-valor2.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
+
+let btSubratrai = document.getElementById("btSubtrair");
+btSubratrai.addEventListener("click", function () {
+    subtrair(Number(valor1.value), Number(valor2.value));
 });
+
+let btMultiplica = document.getElementById("btMultiplicar");
+btMultiplica.addEventListener("click", function () {
+    multiplicar(Number(valor1.value), Number(valor2.value));
+});
+
+let btDividi = document.getElementById("btDividir");
+btDividi.addEventListener("click", function () {
+    dividir(Number(valor1.value), Number(valor2.value));
+});
+
 
 function somar(a, b) {
-    let soma = a + b;
-    resultado.innerHTML = soma;
-}
+    resultado.textContent = a + b;
+};
+
+function subtrair(a, b) {
+    resultado.textContent = a - b;
+};
+
+function multiplicar(a, b) {
+    resultado.textContent = a * b;
+};
+
+function dividir(a, b) {
+    resultado.textContent = a / b;
+};
